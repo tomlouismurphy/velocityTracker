4a51eda6cf0a16a4d2f748be011df9713d429d87
tom.louis.murphy <tom.louis.murphy@gmail.com>
2018-05-09 23:45:21 -0500

    adding display functionality menus to each pitcher

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,7 @@
 const populatePitcherList = () => {
 	for (let i = 0; i < startingPitchers.length; i++){
 		const $newdiv = $('<div></div>');
+		$newdiv.addClass('soxPitcher');
 		const $newtext = $('<p></p>');
 		$newtext.text(startingPitchers[i].name);
 		$newdiv.append($newtext);
@@ -12,3 +13,15 @@ const populatePitcherList = () => {
 }
 
 populatePitcherList();
+
+const viewGamesList = () => {
+	$('.soxPitcher').on('click', (e) => {
+		const $newdiv = $('<div></div>');
+		const $newtitle = $('<h4></h4>');
+		$newtitle.text(e.target.innerText);
+		$newdiv.append($newtitle);
+		$('.selectedPitcher').append($newdiv);
+	})
+}
+
+viewGamesList();
--- scripts/games.js
+++ scripts/games.js
@@ -2,5 +2,10 @@ class Game {
 	constructor(date, opponent){
 		this.date = date;
 		this.opponent = opponent;
+		this.pitchesThrown = [];
 	}
 }
+
+const may032018 = new Game('May 3, 2018', 'Texas Rangers');
+
+price.gamesStarted.push(may032018);
