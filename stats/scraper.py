eb49e8a4996a318183f401507e02f0d83f0c18b7
tom.louis.murphy <tom.louis.murphy@gmail.com>
2018-05-15 23:29:38 -0500

    fashioning python scraper to access baseball stats

---
+++ stats/scraper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+page = requests.get('http://www.brooksbaseball.net/pfxVB/tabdel_expanded.php?pitchSel=456034&game=gid_2018_05_03_bosmlb_texmlb_1/&s_type=3&h_size=700&v_size=500')
+
+data = page.text
+
+soup = BeautifulSoup(data, 'html5lib')
+
+whole_text = str(soup)
+
+pitch_list = whole_text.split('2018-05-04')
+
+pitch_list.pop(0)
+
+print(pitch_list[85])
+
+print(len(pitch_list))
